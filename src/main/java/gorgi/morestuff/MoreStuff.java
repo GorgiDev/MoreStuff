8d1668dd03f8fac2f79d48298465ade1b6fbb8cd
GorgiDev <gkaragiozis24@gmail.com>
2026-03-21 16:56:05 +0200

    Initial commit

---
+++ src/main/java/gorgi/morestuff/MoreStuff.java
@@ -0,0 +1,16 @@
+package gorgi.morestuff;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoreStuff implements ModInitializer {
+	public static final String MOD_ID = "morestuff";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/gorgi/morestuff/MoreStuffClient.java
@@ -0,0 +1,10 @@
+package gorgi.morestuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreStuffClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
